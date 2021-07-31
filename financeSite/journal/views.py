badea75309535eaa47e18aea7947547e5151896a
Michael Maranan <michaelmaranan030@gmail.com>
2021-07-31 22:32:56 +0800

    base view

--- financeSite/journal/views.py
+++ financeSite/journal/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import debit,credit
 
 # Create your views here.
+
 def index(response):
-    return HttpResponse('hello world')
+    jl = debit.objects
+    return render(response,'journal/index.html',{'jl':jl})
+
+# plan and make the free fill journal site
