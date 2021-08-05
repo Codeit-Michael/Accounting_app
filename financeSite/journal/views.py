45e528d9c0c4997fb53d1f4ced4980a8b89bbec4
Michael Maranan <michaelmaranan030@gmail.com>
2021-08-05 09:46:24 +0800

    fixed ledgers

--- financeSite/journal/views.py
+++ financeSite/journal/views.py
@@ -19,8 +19,8 @@ def index(response,id):
 
         try:
             if len(dtext) > 0 and int(amt) > 0 and len(ctext) > 0:
-                db = cnt.debit_set.create(dbt=dtext,dbt_amount=int(amt))
-                cd = db.credit_set.create(cdt=ctext,cdt_amount=int(amt))
+                db = cnt.debit_set.create(dbt=dtext,dbt_amount=amt)
+                cd = db.credit_set.create(cdt=ctext,cdt_amount=amt)
                 db.save()
                 cd.save()
             else:
@@ -45,6 +45,7 @@ def create(response):
 def ledger(response,id):
     cnt = my_journal.objects.get(id=id)
     t_accounts = {}
+    trial_balance = {}
 
     # yet working
     for item in cnt.debit_set.all():
@@ -54,13 +55,25 @@ def ledger(response,id):
             t_accounts[item.dbt] = {}
             t_accounts[item.dbt]['debit'] = []
             t_accounts[item.dbt]['credit'] = []
+            trial_balance[item.dbt] = {}
+            trial_balance[item.dbt]['debit'] = 0
+            trial_balance[item.dbt]['credit'] = 0
         
         if newItem.cdt not in t_accounts:
             t_accounts[newItem.cdt] = {}
             t_accounts[newItem.cdt]['debit'] = []
             t_accounts[newItem.cdt]['credit'] = []
+            trial_balance[newItem.cdt] = {}
+            trial_balance[newItem.cdt]['debit'] = 0
+            trial_balance[newItem.cdt]['credit'] = 0
 
-        t_accounts[item.dbt]['debit'].append(int(item.dbt_amount))
-        t_accounts[newItem.cdt]['credit'].append(int(newItem.cdt_amount))
-        
+        t_accounts[item.dbt]['debit'].append(item.dbt_amount)
+        t_accounts[newItem.cdt]['credit'].append(newItem.cdt_amount)
+        trial_balance[item.dbt]['debit'] += item.dbt_amount
+        trial_balance[newItem.cdt]['credit'] += newItem.cdt_amount
+
+    print(trial_balance)
+
     return render(response, 'journal/ledgers.html', {'t_accounts':t_accounts})
+
+    """ FIX THE SUM OF DEBITS AND CREDIT FOR EACH TRANSACTION """
