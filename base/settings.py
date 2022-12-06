b833baf0e21fb015b3980303e74cfbd49e41f615
Codeit-Michael <michaelmaranan030@gmail.com>
2022-12-07 03:01:00 +0800

    add css for delete and create pfl

--- base/settings.py
+++ base/settings.py
@@ -118,6 +118,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = "/var/www/example.com/static/"
+
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
     BASE_DIR / "static",
