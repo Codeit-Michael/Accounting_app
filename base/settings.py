7a6f50977a559ddf9865301f9f12b48c54786178
Michael Maranan <michaelmaranan030@gmail.com>
2023-01-23 01:55:08 +0800

    replace css w/ scss

--- base/settings.py
+++ base/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app.apps.AppConfig',
+    'compressor',
 ]
 
 MIDDLEWARE = [
@@ -117,11 +118,21 @@ LOGIN_URL = 'login'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "templates/staticfiles")]
+STATIC_ROOT = os.path.join(BASE_DIR, 'app/static')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_FINDERS = [
+    'compressor.finders.CompressorFinder',
+]
+
+COMPRESS_PRECOMPILERS = (
+    ('text/x-scss', 'django_libsass.SassCompiler'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
