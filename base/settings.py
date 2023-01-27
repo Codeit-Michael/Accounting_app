88a832ee253fd85ceaa8e7513d0cd1568b0141b2
Michael Maranan <michaelmaranan030@gmail.com>
2023-01-27 21:39:52 +0800

    replace scss with css link

--- base/settings.py
+++ base/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app.apps.AppConfig',
-    'compressor',
 ]
 
 MIDDLEWARE = [
@@ -120,19 +119,19 @@ LOGIN_URL = 'login'
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'app/static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'app/static')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
-STATICFILES_FINDERS = [
-    'compressor.finders.CompressorFinder',
-]
+# STATICFILES_FINDERS = [
+#     'compressor.finders.CompressorFinder',
+# ]
 
-COMPRESS_PRECOMPILERS = (
-    ('text/x-scss', 'django_libsass.SassCompiler'),
-)
+# COMPRESS_PRECOMPILERS = (
+#     ('text/x-scss', 'django_libsass.SassCompiler'),
+# )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
